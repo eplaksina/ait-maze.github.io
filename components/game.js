64bff85000f4ebbc69a2c5766229a49d20d61bf5
eplaksina_angelsit <eplaksina@angelsit.ru>
2022-10-25 18:06:39 +0300

    feat: add animation gifts near tree

--- components/game.js
+++ components/game.js
@@ -28,7 +28,7 @@ function hideGift(e) {
 function setVisible(gift, cameraPos, x1, x2, z1, z2) {
     let giftPosition = gift.getAttribute('position')
     if (giftPosition.z + z1 >= cameraPos.z && giftPosition.z + z2 <= cameraPos.z && giftPosition.x + x1 >= cameraPos.x && giftPosition.x + x2 <= cameraPos.x) {
-        if (gift.getAttribute('scale').x == 3 && i != 1) {
+        if ((gift.getAttribute('scale').x == 3 || gift.getAttribute('scale').x == 4) && i != 1) {
             i = 0
             magicSound.play()
             counterPoints++
@@ -36,8 +36,6 @@ function setVisible(gift, cameraPos, x1, x2, z1, z2) {
         }
 
         gift.setAttribute('animation', { 'property': 'scale', "to": { x: 0.001, y: 0.001, z: 0.001 }, "loop": false, "dur": 200 })
-/*         setTimeout(gift.setAttribute('visible', false), 200)
- */
         i++
     }
 }
@@ -55,9 +53,25 @@ function giveGift(cameraPos) {
     let treePosition = tree.getAttribute('position')
 
     let catImg = document.getElementsByClassName('end-game')[0]
+    let treeGift1 = document.getElementById('treeGift1')
+    let treeGift2 = document.getElementById('treeGift2')
+    let treeGift3 = document.getElementById('treeGift3')
+    let treeGift4 = document.getElementById('treeGift4')
+    let treeGift5 = document.getElementById('treeGift5')
+    let treeGift6 = document.getElementById('treeGift6')
 
-    if (treePosition.z + 50 >= cameraPos.z && treePosition.z - 50 <= cameraPos.z && treePosition.x + 50 >= cameraPos.x && treePosition.x - 50 <= cameraPos.x && takenGifts.innerText == 6) {
-        catImg.classList.add('visible')
+    if (treePosition.z + 55 >= cameraPos.z && treePosition.z - 55 <= cameraPos.z && treePosition.x + 55 >= cameraPos.x && treePosition.x - 55 <= cameraPos.x && takenGifts.innerText == 0) {
+
+        treeGift4.setAttribute('animation', { 'property': 'scale', "to": { x: 2.2, y: 2.2, z: 2.2 }, "loop": false, "dur": 200 })
+
+        setTimeout(() => { treeGift2.setAttribute('animation', { 'property': 'scale', "to": { x: 1.8, y: 1.8, z: 1.8 }, "loop": false, "dur": 200 }) }, 200)
+        setTimeout(() => { treeGift1.setAttribute('animation', { 'property': 'scale', "to": { x: 1.5, y: 1.5, z: 1.5 }, "loop": false, "dur": 200 }) }, 400)
+        setTimeout(() => { treeGift3.setAttribute('animation', { 'property': 'scale', "to": { x: 1, y: 1, z: 1 }, "loop": false, "dur": 200 }) }, 600)
+        setTimeout(() => { treeGift5.setAttribute('animation', { 'property': 'scale', "to": { x: 1.5, y: 1.5, z: 1.5 }, "loop": false, "dur": 200 }) }, 800)
+        setTimeout(() => { treeGift6.setAttribute('animation', { 'property': 'scale', "to": { x: 1.8, y: 1.8, z: 1.8 }, "loop": false, "dur": 200 }) }, 1000)
+
+        setTimeout(() => { catImg.classList.add('visible') }, 1200)
+        
     }
 }
 
